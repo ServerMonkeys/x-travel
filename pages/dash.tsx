a47e8651dc5d73f340e83d5ef73ff4f0ab935c21
Chris Leyva <cleyvamendez@protonmail.com>
2022-10-13 14:27:12 -0700

    added a refresh button with state

--- pages/dash.tsx
+++ pages/dash.tsx
@@ -7,71 +7,97 @@ import fetchData, {
   fetchDOS_Query,
   fetchSS_Query
 } from "../helper/fetchData";
+
 import {SearchForm} from '../components/SearchForm'
 import {motion} from 'framer-motion'
 import React, {useEffect, useState} from 'react'
 import {BaseCard} from '../components/Card/Card'
-import Notification from '../components/Verified'
 
 export const Dash: NextPage = () => {
 
+  const [button_press, Press] = useState<boolean>(false);
   const [SSN, setSSN] = useState<number>()
   const [SSstatus, setSSStatus] = useState<string>('ready')
   const [DMVStatus, setDMVStatus] = useState<string>('ready')
   const [DOSStatus, setDOSStatus] = useState<string>('ready')
 
-
   const [DOS_data, setDOS_data] = useState([]);
   const [SS_data, setSS_data] = useState([]);
   const [DMV_data, setDMV_data] = useState([]);
 
-
-  const processData = (data:any) => {
-    console.log(data)
-  }
-
-  const getData = async (client:any, query: any, ssn: number, setState: any, setData:any) => {
-    setState('loading')
-    return await fetchData(client, query, ssn)
-        .then(data =>{
-          if(data) {
-            setState('fetched')
-            setData(data);
-            processData(data)
-          }else {
-            setState('error')
-          }
-        })
-  }
   useEffect(() => {
     if (!SSN) return
-    getData(clientDOS, fetchDOS_Query, SSN, setDOSStatus, setDOS_data)
-    getData(clientSS, fetchSS_Query, SSN, setSSStatus, setSS_data)
-    getData(clientDMV, fetchDMV_Query, SSN, setDMVStatus, setDMV_data)
-  }, [SSN])
+    const fetchMyData = async(): Promise<void> => {
+      const getData = async (client:any, query: any, ssn: number, setState: any, setData:any) => {
+        setState('loading')
+        await Promise.resolve(fetchData(client, query, ssn))
+            .then(data =>{
+              if(data) {
+                setState('fetched')
+                setData(data);
+              }else {
+                setState('error')
+              }
+            })
+      }
+      await Promise.all([
+        getData(clientSS, fetchSS_Query, SSN, setSSStatus, setSS_data),
+        getData(clientDOS, fetchDOS_Query, SSN, setDOSStatus, setDOS_data),
+        getData(clientDMV, fetchDMV_Query, SSN, setDMVStatus, setDMV_data)
+      ])
+    }
+    fetchMyData()
+        .finally(() => {
+          console.log('queried all data')
+        })
+  }, [SSN,button_press])
 
   return (
-    <div className={'w-full h-fit  bg-gradient-to-r from-stop1 to-stop2 to-stop3 bg-no-repeat bg-bottom '}>
-      <div className="min-h-screen flex flex-1 flex-col justify-center items-center box-border relative   bg-top-center bg-no-repeat  ">
-        <motion.div
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
-          className="grid relative grid-cols-3 auto-rows-auto gap-10 overflow-visible "
-        >
-          <BaseCard agencyFullName={'Department of State'} status={DOSStatus} />
-          <BaseCard agencyFullName={'Social Security'} status={SSstatus} />
-          <BaseCard agencyFullName={'Dept Motor Vehicles'} status={DMVStatus} />
-        </motion.div>
-        <SearchForm onChange={(e: any) =>{
-          setSSN(e);
-        }} />
-        <p>{SSN}</p>
-        <div className=" invisible ease-in-out duration-200">
-          <Notification name="Nelson" occupation="President" />
+      <div className='w-full h-fit content-center bg-gradient-to-r from-stop1 to-stop2 to-stop3 bg-no-repeat bg-bottom flex flex-col justify-items-start'>
+        <div className="relative box-border flex min-h-screen flex-1 flex-col items-center justify-center bg-no-repeat">
+          <motion.div
+              initial={{opacity:0, scale:1, y:-100}}
+              animate={{ opacity:1, scale:1, y:0}}
+              transition={{ duration:0.75}}
+              className="relative grid auto-rows-auto grid-cols-3 gap-10 overflow-visible"
+          >
+            <BaseCard agencyFullName={'Department of State'} status={DOSStatus} />
+            <BaseCard agencyFullName={'Social Security'} status={SSstatus} />
+            <BaseCard agencyFullName={'Dept Motor Vehicles'} status={DMVStatus} />
+          </motion.div>
+          <SearchForm onChange={(e: any) =>{
+            setSSN(e);
+          }} />
+          <motion.button
+              initial={{opacity:0, scale: 0.5}}
+              animate={{opacity:1, scale:1}}
+              transition={{duration:0.8}}
+              type="button"
+              className="inline-flex items-center justify-center w-auto px-2 py-2 space-x-2 text-sm
+                  font-medium text-white bg-blue-700 border border-blue-700 rounded-full
+                  appearance-none cursor-pointer select-none hover:border-blue-800
+                  focus:outline-none focus:ring-2 focus:ring-blue-300  disabled:opacity-75"
+              whileHover={{
+                scale:1.05
+              }}
+              whileTap={{
+                scale: 0.9,
+                rotate: 360,
+                transition: { type: "spring", stiffness: 100, duration:0.9 },
+              }}
+              onClick={() => {
+                Press(!button_press)
+              }}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" viewBox="0 0 20 20"
+                 fill="currentColor">
+              <path fillRule="evenodd"
+                    d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z"
+                    clipRule="evenodd"/>
+            </svg>
+          </motion.button>
         </div>
       </div>
-    </div>
   )
 }
 export default Dash
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,12 @@
 import Link from 'next/link'
 import DemoCard from "../components/Card/demoCard";
-import { WorldView } from '../components/threejs/world'
-import AvatarIcon from '../components/Avatar/AvatarIcon'
-import Avatar from '../components/Avatar'
-import { motion } from 'framer-motion'
-
-const imgUrl =
-  'https://www.csus.edu/university-advancement/university-foundation/meet-us/_internal/photos/robert-nelsen.jpg'
+import {WorldView} from '../components/threejs/world'
+import {motion} from 'framer-motion'
 
 export const Home = () => {
   return (
-    <div className="font-Manrope min-h-screen flex flex-1 flex-col justify-start items-center box-border relative transition ease-in-out delay-150 bg-gradient-to-r from-stop1 to-stop2 to-stop3">
-      <div className="flex w-full h-800 overflow-hidden bg-black relative">
+    <div className="relative box-border flex min-h-screen flex-1 flex-col items-center justify-start bg-gradient-to-r transition delay-150 ease-in-out font-Manrope from-stop1 to-stop2 to-stop3">
+      <div className="relative flex w-full overflow-hidden bg-black h-800">
         <div className="w-fit h-fit bg-no-repeat text-center flex items-center absolute flex
          mx-auto top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 text-align
          items-center
@@ -23,13 +18,13 @@ export const Home = () => {
                       transition={{ delay: 1.5 }}>
             <DemoCard/>
           </motion.div>
-          <div id="card container" className="w-full h-fit p-3 flex flex-col gap-8 rounded-2xl m-5 z-10 ">
+          <div id="card container" className="z-10 m-5 flex h-fit w-full flex-col gap-8 rounded-2xl p-3">
             <motion.h1
                 unselectable={'on'}
               initial={{ opacity: 0 }}
               whileInView={{ opacity: 1 }}
               transition={{ delay: 1 }}
-              className="font-bold text-8xl  text-neutral-800 z-10"
+              className="z-10 text-8xl font-bold text-neutral-800"
             >
               travel.x makes it <span className="text-green-300">easy.</span>
             </motion.h1>
@@ -39,28 +34,28 @@ export const Home = () => {
       </div>
 
 
-      <div className='bg-black  h-screen w-full  justify-center items-center box-border relative flex flex-col pointer-events-none'>
-        <div className=" m-6 gap-12 flex flex-row items-center justify-center overflow-visible position relative p-16">
-          <div className="text-center font-Manrope text-white ">
+      <div className='pointer-events-none relative box-border flex h-screen w-full flex-col items-center justify-center bg-black'>
+        <div className="relative m-6 flex flex-row items-center justify-center gap-12 overflow-visible p-16 position">
+          <div className="text-center text-white font-Manrope">
             <motion.h1
                 initial={{ opacity: 0 }}
                 whileInView={{ opacity: 1 }}
                 transition={{ delay: 1 }}
                 className="text-6xl font-bold"
             >
-              <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+              <span className="bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent">
                 Less Hassle
               </span>
-              <p className=" text-2xl">more going to where you need to go</p>
+              <p className="text-2xl">more going to where you need to go</p>
             </motion.h1>
 
           </div>
-          <div className=" text-center font-Manrope text-white">
+          <div className="text-center text-white font-Manrope">
             <motion.h1
                 initial={{ opacity: 0 }}
                 whileInView={{ opacity: 1 }}
                 transition={{ delay: 1.5 }}
-                className=" text-6xl font-bold">
+                className="text-6xl font-bold">
               Your Identity <span className="text-green-300">is</span> your passport
             </motion.h1>
           </div>
@@ -68,17 +63,17 @@ export const Home = () => {
         <div className='Banner-Hero bg-black h-[80rem] w-[100rem] justify-start flex flex-col text-center items-center overflow-hidden'>
           <div className={'w-w-img h-h-img flex flex-col items-center'}>
             <div className={'bg-opacity-50 '}>
-              <h1 className="font-Manrope text-4xl font-medium m-6 text-white"></h1>
+              <h1 className="m-6 text-4xl font-medium text-white font-Manrope"></h1>
             </div>
             <div className=" w-[75rem] h-[40rem] bg-hero-banner bg-contain bg-no-repeat flex flex-col-reverse bg-center justify-center ">
-              <h2 className=" font-bold text-white text-6xl font-Manrope mt-44 hover:opacity-90">
-                <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+              <h2 className="mt-44 text-6xl font-bold text-white font-Manrope hover:opacity-90">
+                <span className="bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent">
                   <Link href={'/dash'}>Secure</Link>
                 </span>
               </h2>
             </div>
-            <div className="text-container w-full text-center">
-              <h1 className="font-Manrope font-bold text-white text-4xl">Click on Secure to get the dashboard</h1>
+            <div className="w-full text-center text-container">
+              <h1 className="text-4xl font-bold text-white font-Manrope">Click on Secure to get the dashboard</h1>
             </div>
           </div>
         </div>
@@ -88,11 +83,11 @@ export const Home = () => {
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
           transition={{ delay: 1 }}
-          className=" w-full h-800 justify-center items-center box-border relative flex flex-col bg-pattern-svg bg-no-repeat bg-cover bg-bottom"
+          className="relative box-border flex w-full flex-col items-center justify-center bg-cover bg-bottom bg-no-repeat h-800 bg-pattern-svg"
       >
-        <div className="text-container justify-center text-center backdrop-blur-sm rounded-md p-4 my-10">
-          <h2 className="my-8 p-3 rounded-full text-5xl text-slate-700 font-Manrope font-bold ">Travel Smart</h2>
-          <div className="w-96 ">
+        <div className="my-10 justify-center rounded-md p-4 text-center backdrop-blur-sm text-container">
+          <h2 className="my-8 rounded-full p-3 text-5xl font-bold text-slate-700 font-Manrope">Travel Smart</h2>
+          <div className="w-96">
             <p className={"text-neutral-800 font-semibold"}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
               dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
