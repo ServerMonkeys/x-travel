d13f150dfe387ac826d8445a06378bf9cac6b5ae
jasjot1 <sjasjot123@gmail.com>
2022-10-20 16:06:57 -0700

    Testing update

--- components/Avatar/AvatarIcon.tsx
+++ components/Avatar/AvatarIcon.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable @next/next/no-img-element */
 
 import Image from 'next/image'
+
 export const AvatarIcon = (imgUrl: any) => {
   return (
     <div className="relative">
-      <Image src={imgUrl} alt={'img-avatar'} />
+      <Image src={imgUrl} alt={'img-avatar'} layout ={'fill'}/>
       <span className="top-0 left-7 absolute  w-3.5 h-3.5 bg-green-400 border-2 border-white dark:border-gray-800 rounded-full"></span>
     </div>
   )
