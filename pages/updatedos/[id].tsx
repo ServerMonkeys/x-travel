9e913387e7f37f77436140552255f52e855f7bdb
jasjot1 <sjasjot123@gmail.com>
2022-10-26 19:03:09 -0700

    Update with DMV and DOS upload

--- pages/updatedos/[id].tsx
+++ pages/updatedos/[id].tsx
@@ -11,6 +11,46 @@ function id() {
     const [passportPhoto,updatePassportPhoto] = useState("");
     const personID = router.query.id
 
+    const [imageSrc, setImageSrc] = useState();
+    const [uploadData, setUploadData] = useState();
+
+
+    function handleOnChange(changeEvent) {
+        const reader = new FileReader();
+
+        reader.onload = function(onLoadEvent) {
+            setImageSrc(onLoadEvent.target.result);
+            setUploadData(undefined);
+        }
+
+
+        reader.readAsDataURL(changeEvent.target.files[0]);
+    }
+
+
+    async function handleOnSubmit(event) {
+        event.preventDefault();
+
+        const form = event.currentTarget;
+        const fileInput = Array.from(form.elements).find(({ name }) => name === 'file');
+
+        const formData = new FormData();
+
+        for ( const file of fileInput.files ) {
+            formData.append('file', file);
+        }
+
+        formData.append('upload_preset', 'my-uploads');
+
+        const data = await fetch('https://api.cloudinary.com/v1_1/dbzeopi6f/image/upload', {
+            method: 'POST',
+            body: formData
+        }).then(r => r.json());
+
+        setImageSrc(data.secure_url);
+        setUploadData(data);
+    }
+
     const handleSubmit = async (event:any) => {
         event.preventDefault();
 
@@ -26,22 +66,10 @@ function id() {
         if(passportExp.length > 0){
             await updatePerson(clientDOS, String(personID), 'passport_exp', String(passportExp));
         }
-        if(passportPhoto.length > 0){
-            await updatePerson(clientDOS, String(personID), 'photo', String(passportPhoto));
+        if(imageSrc){
+            await updatePerson(clientDOS, String(personID), 'photo', String(imageSrc));
         }
         await Router.push({pathname:"/dash"});
-        /*try {
-            const {data} = await axios({
-                url: "/api/update",
-                method: "POST",
-                data: teamPayload
-            });
-            console.log("response back", data)
-        }
-        catch(error) {
-            console.log("Error", error);
-
-        }*/
     }
 
 
@@ -61,16 +89,14 @@ function id() {
 
                 >
                     <div className={`dark:text-slate-400  rounded-xl p-1 w-fit h-fit m-1 select-none cursor-pointer flex flex-col justify-center items-center`}>
-                        <h2 className="text-blue-500 dark:text-white drop-shadow-md px-4 py-2 font-semibold text-2xl w-full">
+                        <h2 className="text-blue-500 dark:text-white drop-shadow-md px-4 py-2 font-semi-bold text-2xl w-full">
                             Update DOS
                         </h2>
-                        <div>
                             <span className="text-gray-500 space-y-8"> Update Passport # </span>
                             <input className="rounded border text-black" minLength={9}
                                    maxLength={9}
                             value={passportNum}
                             onChange={({target}) => updatePassportNum(target?.value)}></input>
-                        </div>
                         <div>
                         <span className="flex text-gray-500"> Update Passport Expiry Date</span>
                             <input className="rounded border text-black"
@@ -82,10 +108,17 @@ function id() {
                         </div>
                         <div>
                             <span className="flex text-gray-500"> Update Passport Photo</span>
-                            <input className="rounded border text-black"
-                                   value={passportPhoto}
-                                   onChange={({target}) => updatePassportPhoto(target?.value)}
-                            ></input>
+                            <form className="text-black" method="post" onChange={handleOnChange} onSubmit={handleOnSubmit}>
+                                <div>
+                                    <input type="file" name="file" accept="image/*"/>
+                                </div>
+
+                                {imageSrc && !uploadData && (
+                                    <div>
+                                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Upload Files</button>
+                                    </div>
+                                )}
+                            </form>
                         </div>
                     </div>
                     <div>
