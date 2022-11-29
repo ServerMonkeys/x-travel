4a42419a2c716f1c82b535b9e6a9d0be2275de41
jasjot1 <sjasjot123@gmail.com>
2022-11-28 18:44:12 -0800

    Added add page for SS

--- pages/ssadd.tsx
+++ pages/ssadd.tsx
@@ -141,7 +141,7 @@ function SSadd() {
                         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                         whileTap={{scale:0.9}}
                         >
-                        Add Person
+                        Submit
 
                 </motion.button>
             </div>
