6b0f0cbe40235760749a7131502d85046adee044
jasjot1 <sjasjot123@gmail.com>
2022-12-12 21:45:41 -0800

    Fixed footer links, and added website description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,15 @@ import type {AppProps} from 'next/app'
 import Navbar from '../components/navbar'
 import Footer from "../components/footer";
 import {UserProvider} from '@auth0/nextjs-auth0'
-import {ThemeProvider} from "next-themes";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-      <ThemeProvider>
         <UserProvider>
           <Navbar />
             <Component {...pageProps} />
             <Footer/>
         </UserProvider>
-      </ThemeProvider>
+
   )
 }
 
